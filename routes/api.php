23b27c79503bd097382c527664b3f5d7b3e067d9
Eladeeb <eslamabdo10000@gmail.com>
2019-11-24 15:00:01 +0200

    wdhytgrfed

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 Route::get('wrecks','Api\WrecksController@index');
-Route::get('effrh');
-Route::get('effrh');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
